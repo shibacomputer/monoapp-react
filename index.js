03c8456d37655d6be42ce36178e212381ad075e1
Cade <cade@shiba.computer>
2020-07-03 19:37:16 +0200

    Render?

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var Preact = require('preact')
 Component.prototype = Object.create(Preact.Component.prototype)
 
 function withReact (state, emitter, app) {
-  app._mount = (tree, newTree) => render(newTree, tree)
-  app._render = (tree, newTree) => render(newTree, tree)
+  app._mount = (tree, newTree) => Preact.render(newTree, tree)
+  app._render = (tree, newTree) => Preact.render(newTree, tree)
   Component.prototype.global = state
   Component.prototype.emit = function () {
     emitter.emit.apply(emitter, arguments)
